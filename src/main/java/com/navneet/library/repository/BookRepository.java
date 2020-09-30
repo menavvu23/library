7fdf196387f0f6149bb202d05ba8f86f2cdb2e2a
menavvu23 <60959718+menavvu23@users.noreply.github.com>
2020-09-30 16:39:49 +0530

    Update BookRepository.java

--- src/main/java/com/navneet/library/repository/BookRepository.java
+++ src/main/java/com/navneet/library/repository/BookRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+//book repository
 @Repository
 public interface BookRepository extends JpaRepository<Book,Integer> {
 
